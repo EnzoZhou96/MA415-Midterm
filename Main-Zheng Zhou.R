80ffd4dbc3bf5af2bddeae6a46aedcc9125e0893
Zheng Zhou <474690697@qq.com>
2017-03-23 01:59:12 -0400

    1

--- Main-Zheng Zhou.R
+++ Main-Zheng Zhou.R
@@ -1,7 +1,7 @@
 #Zheng Zhou
 #MA415-Midterm Project
 
-#Load 10 packages
+#Load 9 packages
 library(tidyr)
 library(dplyr)
 library(foreign)
@@ -10,26 +10,54 @@ library(lubridate)
 library(stringr)
 library(data.table)
 library(ggplot2)
-library(scale)
 library(zipcode)
+library(png)
 
 #Load target data tables
 osha <- read.dbf("osha.dbf")
 accid <- read.dbf("accid.dbf")
+viol <- read.dbf("viol.dbf")
 
-#Construct table "new" that consists of two columns: sitezip , totserious.
-new <- data.frame(osha$SITEZIP,osha$TOTSERIOUS)
+#Load look up tables
+scc_1 <- read.dbf("lookups/scc.dbf")
 
-#Arrange table by zipcode in decreasing order.
-new2 <- arrange(new,desc(osha.SITEZIP))
+#Check dimensions for each table
+dim(osha)
+dim(accid)
+dim(viol)
 
-#Modify the table to filter out zip codes that do not belong to MA or are missing.
-new3 <- new2[136:80384,]
+#Prepare osha
+#I am interested in the relations between  (site county) and (total number of serious incident).
+#Construct table "new" that consists of two columns: sitecnty , totserious.
+new <- data.frame(osha$TOTSERIOUS,osha$SITECNTY,osha$SITECITY,osha$SITEZIP)
 
-#visulize new3
-#Goal：finding the area that reports most serious violations
+new1 <- arrange(new,desc(osha.SITEZIP))
+#Filter scc to MA only
+scc_MA <- subset(scc_1, grepl("MA",scc_1$STATE))
 
 
+#visulize 
+#Goal：finding the area that reports most serious violation.
+
+zipcode_1 <- data(zipcode)
+zipcode_MA <- subset(zipcode, grepl("MA", zipcode$state))
+
+new2 <- data.frame(TOTSERIOUS=new1$osha.TOTSERIOUS,zip = new1$osha.SITEZIP)
+
+new3 <- inner_join(new2,zipcode_MA, by = "zip")
+
+
+ma <- readPNG("MA.png") 
+
+ggplot(new3, aes(y = latitude, x = longitude)) 
++ geom_point(aes(size = TOTSERIOUS,colour = TOTSERIOUS))
++ annotation_custom(rasterGrob(ma, 
+                               width = unit(1,"npc"), 
+                               height = unit(1,"npc")), 
+                    -Inf, Inf, -Inf, Inf)
+
+
+#Prepareaccid
 
 
 
